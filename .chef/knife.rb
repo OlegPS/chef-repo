fa3ab720f843de88151bfce510abfac946e9fa09
Oleg Selin <oleg.selin@gmail.com>
2014-08-08 16:17:12 +0400

    Added server certificate

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -4,6 +4,7 @@ node_name                'oleg'
 client_key               '/home/oleg/.chef/oleg.pem'
 validation_client_name   'chef-validator'
 validation_key           '/home/oleg/.chef/keys/chef-validator.pem'
-chef_server_url          'https://phy1mg2.local:443'
+chef_server_url          'https://phy1mg2:443'
+ssl_verify_mode          'verify_peer'
 syntax_check_cache_path  '/home/oleg/.chef/syntax_check_cache'
-cookbook_path [ 'https://github.com/OlegPS/chef-repo.git/cookbooks' ]
+cookbook_path            [ 'cookbooks', 'site-cookbooks' ]
